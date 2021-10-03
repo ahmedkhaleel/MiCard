b8caced48279f06388476027607fbe79c2dd0682
ahmedkhaleel <duZhuqkyptu8barnuz>
2021-10-03 22:13:53 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+   MyApp()
   );
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+              child: Column(
+                children: [
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+
+                    color: Colors.white,
+                    child: Text("Container 1"),
+                  ),
+
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+
+                    color: Colors.white,
+                    child: Text("Container 1"),
+                  ),
+                ],
+              ),
+        ),
+
+    )
+      );
+  }
 }
